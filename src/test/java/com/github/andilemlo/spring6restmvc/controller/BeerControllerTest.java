861355c36226b5c2687f985ae097f6179824500b
AndileMlo <138500253+AndileMlo@users.noreply.github.com>
2023-07-18 16:20:57 +0200

    Return Data with Mockito
    equipped the Mock MVC controller to be able to return an object

--- src/test/java/com/github/andilemlo/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/github/andilemlo/spring6restmvc/controller/BeerControllerTest.java
@@ -1,6 +1,8 @@
 package com.github.andilemlo.spring6restmvc.controller;
 
+import com.github.andilemlo.spring6restmvc.model.Beer;
 import com.github.andilemlo.spring6restmvc.services.BeerService;
+import com.github.andilemlo.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,7 +13,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,6 +29,9 @@ class BeerControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
+
     @Autowired
     BeerController beerController;
 
@@ -33,8 +41,13 @@ class BeerControllerTest {
     @Test
     void getBeerbyId() throws Exception {
 
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+
+
+        given(beerService.getBeerByID(any(UUID.class))).willReturn(testBeer);
+
         mockMvc.perform(get("/api/v1/beer/" +  UUID.randomUUID())
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
