780d146681f76578b278d948e47d7fd78491bf1c
AndileMlo <138500253+AndileMlo@users.noreply.github.com>
2023-08-02 15:27:27 +0200

    Fix Integration Tests

--- src/test/java/com/github/andilemlo/spring6restmvc/bootstrap/BootStrapDataTest.java
+++ src/test/java/com/github/andilemlo/spring6restmvc/bootstrap/BootStrapDataTest.java
@@ -4,15 +4,18 @@ import com.github.andilemlo.spring6restmvc.entities.Beer;
 import com.github.andilemlo.spring6restmvc.repositories.BeerRepository;
 import com.github.andilemlo.spring6restmvc.repositories.CustomerRepository;
 import com.github.andilemlo.spring6restmvc.services.BeerCsvService;
+import com.github.andilemlo.spring6restmvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootStrapDataTest {
 
     @Autowired
@@ -34,7 +37,7 @@ class BootStrapDataTest {
     @Test
     void run()throws Exception {
         bookStrapData.run(null);
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
         assertThat(customerRepository.count()).isEqualTo(3);
 
     }
