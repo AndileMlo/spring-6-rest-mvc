a949b6323ff5439fe3eb988e7083d5d89f073b03
AndileMlo <138500253+AndileMlo@users.noreply.github.com>
2023-08-03 16:08:01 +0200

    Find By Name With Spring Data JPA

--- src/test/java/com/github/andilemlo/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/github/andilemlo/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,23 +1,45 @@
 package com.github.andilemlo.spring6restmvc.repositories;
 
+import com.github.andilemlo.spring6restmvc.bootstrap.BootStrapData;
 import com.github.andilemlo.spring6restmvc.controller.NotFoundException;
 import com.github.andilemlo.spring6restmvc.entities.Beer;
 import com.github.andilemlo.spring6restmvc.model.BeerStyle;
+import com.github.andilemlo.spring6restmvc.services.BeerCsvServiceImpl;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import({BootStrapData.class, BeerCsvServiceImpl.class })
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testGetBeerListByName() {
+        List<Beer> list = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+        assertThat(list.size()).isEqualTo(336);
+
+    }
+
+
+
+    /*
+@Test
+    void testGetBeerListByName() {
+        List<Beer> list = beerRepository.findAllBeerNameIsLikeIgnoreCase("%IPA%");
+
+        assertThat(list.size()).isEqualTo(336);
+    }*/
 
     @Test
     void testSaveBeerNameTooLong(){
