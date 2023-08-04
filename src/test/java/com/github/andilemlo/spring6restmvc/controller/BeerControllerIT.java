33b270d51c74ae301f4bd7ad93663180f3781cd4
AndileMlo <138500253+AndileMlo@users.noreply.github.com>
2023-08-04 15:22:06 +0200

    Add Paging Parameters

--- src/test/java/com/github/andilemlo/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/com/github/andilemlo/spring6restmvc/controller/BeerControllerIT.java
@@ -64,6 +64,20 @@ class BeerControllerIT {
     }
 
     @Test
+    void testListBeersByStyleAndNameShowInventoryTruePage2() throws Exception {
+        mockMvc.perform(get(BeerController.BEER_PATH)
+                        .queryParam("beerName","IPA")
+                        .queryParam("beerStyle", BeerStyle.IPA.name())
+                        .queryParam("showInventory", "true")
+                        .queryParam("pageNumber", "2")
+                        .queryParam("pageSize","50"))
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(50)))
+                .andExpect(jsonPath("$.[0].quantityOnHand").value(IsNull.notNullValue()));
+     }
+
+    @Test
     void testListBeersByStyleAndNameShowInventoryFalse() throws Exception {
         mockMvc.perform(get(BeerController.BEER_PATH)
                         .queryParam("beerName","IPA")
@@ -72,7 +86,6 @@ class BeerControllerIT {
                 .andExpect(status().isOk())
                  .andExpect(jsonPath("$.size()", is(310)))
                 .andExpect(jsonPath("$.[0].quantityOnHand").value(IsNull.nullValue()));
-        ;
     }
 
     @Test
@@ -84,7 +97,6 @@ class BeerControllerIT {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()", is(310)))
                 .andExpect(jsonPath("$.[0].quantityOnHand").value(IsNull.notNullValue()));
-        ;
     }
 
     @Test
@@ -237,7 +249,7 @@ class BeerControllerIT {
 
     @Test
     void testListBeers() {
-        List<BeerDTO> dtos = beerController.listBeers(null, null, false);
+        List<BeerDTO> dtos = beerController.listBeers(null, null, false, 1, 25);
          assertThat(dtos.size()).isEqualTo(2413);
     }
 
@@ -248,7 +260,7 @@ class BeerControllerIT {
     void testEmptyBeers() {
         beerRepository.deleteAll();
 
-        List<BeerDTO> dtos = beerController.listBeers(null, null, false);
+        List<BeerDTO> dtos = beerController.listBeers(null, null, false, 1, 25);
         assertThat(dtos.size()).isEqualTo(0);
     }
 }
--- src/test/java/com/github/andilemlo/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/github/andilemlo/spring6restmvc/controller/BeerControllerTest.java
@@ -70,7 +70,7 @@ class BeerControllerTest {
     @Test
     void testUpdateBeerNullBeerName() throws Exception {
 
-        BeerDTO beerDto =beerServiceImpl.listBeers(null, null, false).get(0);
+        BeerDTO beerDto =beerServiceImpl.listBeers(null, null, false, 1, 25).get(0);
         beerDto.setBeerName("");
 
         given(beerService.updateBeerById(any(),any())).willReturn(Optional.of(beerDto));
@@ -89,7 +89,7 @@ class BeerControllerTest {
     void testCreateBeerNullBeerName() throws Exception{
         BeerDTO beerDTO = BeerDTO.builder().build();
 
-        given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(beerServiceImpl.listBeers(null, null, false).get(1));
+        given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(beerServiceImpl.listBeers(null, null, false, 1, 25).get(1));
 
         MvcResult mvcResult = mockMvc.perform(post(BeerController.BEER_PATH)
                 .accept(MediaType.APPLICATION_JSON)
@@ -123,7 +123,7 @@ class BeerControllerTest {
 
     @Test
     void testPatchBeers() throws Exception{
-        BeerDTO beer = beerServiceImpl.listBeers(null, null, false).get(0);
+        BeerDTO beer = beerServiceImpl.listBeers(null, null, false, 1, 25).get(0);
 
         Map<String, Object> beerMap = new HashMap<>();
         beerMap.put("beerName", "New Name");
@@ -143,8 +143,8 @@ class BeerControllerTest {
     @Test
 
     void testListBeers() throws Exception {
-        given(beerService.listBeers(any(), any(), any()))
-                .willReturn(beerServiceImpl.listBeers(null,null, false));
+        given(beerService.listBeers(any(), any(), any(), any(), any()))
+                .willReturn(beerServiceImpl.listBeers(null,null, false, 1, 25));
 
         mockMvc.perform(get(BeerController.BEER_PATH)
                         .accept(MediaType.APPLICATION_JSON))
@@ -156,7 +156,7 @@ class BeerControllerTest {
 
     @Test
     void testDeleteBeer() throws Exception {
-        BeerDTO beer = beerServiceImpl.listBeers(null,null, false).get(0);
+        BeerDTO beer = beerServiceImpl.listBeers(null,null, false, 1, 25).get(0);
 
 
         given(beerService.deleteBeerById(any())).willReturn(true);
@@ -174,7 +174,7 @@ class BeerControllerTest {
     @Test
     void testUpdateBeer() throws Exception {
 
-        BeerDTO beer = beerServiceImpl.listBeers(null, null, false).get(0);
+        BeerDTO beer = beerServiceImpl.listBeers(null, null, false, 1, 25).get(0);
 
         given(beerService.updateBeerById(any(),any())).willReturn(Optional.of(beer));
 
@@ -193,12 +193,12 @@ class BeerControllerTest {
     @Test
     void testCreateNewBeer() throws Exception {
 
-        BeerDTO beer = beerServiceImpl.listBeers(null, null, false).get(0);
+        BeerDTO beer = beerServiceImpl.listBeers(null, null, false, 1, 25).get(0);
         beer.setVersion(null);
         beer.setId(null);
 
         given(beerService.saveNewBeer(any(BeerDTO.class)))
-                .willReturn(beerServiceImpl.listBeers(null,null, false).get(1));
+                .willReturn(beerServiceImpl.listBeers(null,null, false, 1, 25).get(1));
 
         mockMvc.perform(post(BeerController.BEER_PATH)
                 .accept(MediaType.APPLICATION_JSON)
@@ -222,7 +222,7 @@ class BeerControllerTest {
         @Test
     void getBeerbyId() throws Exception {
 
-        BeerDTO testBeer = beerServiceImpl.listBeers(null, null, false).get(0);
+        BeerDTO testBeer = beerServiceImpl.listBeers(null, null, false, 1, 25).get(0);
 
 
         given(beerService.getBeerById(testBeer.getId())).willReturn(Optional.of(testBeer));
