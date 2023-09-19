1861ea64ca0a7a2b70feaf73f4c942dbb147d6d9
AndileMlo <138500253+AndileMlo@users.noreply.github.com>
2023-09-19 12:58:39 +0200

    Opened up endpoints exposed by Springdoc
    The json can be viewed at http://localhost:8081/v3/api-docs, the yaml can be downloaded at http://localhost:8081/v3/api-docs.yaml, and the swagger ui formatted page can be viewed at http://localhost:8081/swagger-ui.html (it is necessary to enter /v3/api-docs in the search box).

--- src/main/java/com/github/andilemlo/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/com/github/andilemlo/spring6restmvc/config/SpringSecConfig.java
@@ -2,7 +2,6 @@ package com.github.andilemlo.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,6 +12,8 @@ public class SpringSecConfig {
     public SecurityFilterChain filter(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests()
+                        .requestMatchers("/v3/api-docs**","/swagger-ui/**","/swagger-ui.html")
+                        .permitAll()
                         .anyRequest().authenticated()
                         .and().oauth2ResourceServer().jwt();
      //   .csrf().ignoringRequestMatchers("/api/**");
