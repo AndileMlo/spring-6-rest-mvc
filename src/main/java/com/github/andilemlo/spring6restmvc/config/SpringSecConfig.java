207285617f3487d7cd335208db34198744a91986
AndileMlo <138500253+AndileMlo@users.noreply.github.com>
2023-09-19 15:58:58 +0200

    Added Spring Security Configuration
    Some advanced Spring configuration as follows:
    Setting the profile to "test" then disabling the Spring security configuration for the "test" profile (SecurityFilterChain).

--- src/main/java/com/github/andilemlo/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/com/github/andilemlo/spring6restmvc/config/SpringSecConfig.java
@@ -2,12 +2,14 @@ package com.github.andilemlo.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecConfig {
 
+    @Profile("!test")
     @Bean
     public SecurityFilterChain filter(HttpSecurity http) throws Exception {
 
